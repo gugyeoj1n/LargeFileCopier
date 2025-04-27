f9359b2b6b4aaa7a363aa6e41b713bc39eb6658d
Woojin Kwak <kwooj2788@kookmin.ac.kr>
2025-04-27 16:54:54 +0900

    [FEAT] 테스트 파일 생성 기능 구현

---
+++ LargeFileCopier/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+class Program
+{
+    static async Task Main()
+    {
+        string sourceFile = "test.dat";
+        int sizeMB = 10240;
+        CreateTestFile(sourceFile, sizeMB);
+    }
+
+    static void CreateTestFile(string path, int sizeMB)
+    {
+        byte[] buffer = new byte[1024 * 1024];
+        Random rnd = new Random();
+
+        using var fs = new FileStream(path, FileMode.Create, FileAccess.Write);
+        for (int i = 0; i < sizeMB; i++)
+        {
+            rnd.NextBytes(buffer);
+            fs.Write(buffer, 0, buffer.Length);
+        }
+    }
+}
